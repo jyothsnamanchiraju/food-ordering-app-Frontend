ea2834f50f11f85f7bda6ac23bbf86aa155dc5eb
exynerve <prajwalshenoy07@gmail.com>
2021-05-23 09:49:24 +0530

    Checkout screen created

---
+++ src/screens/checkout/checkout.js
@@ -0,0 +1,24 @@
+import {Component} from 'react';
+import './checkout.css';
+
+class Checkout extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+
+        };
+    }
+    render(){
+        return(
+            <div>
+                <div className="delivery-and-payment">
+                    
+                </div>
+                <div className="summary">
+
+                </div>
+            </div>
+        )
+    }
+}
